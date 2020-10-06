2bbd359675bcc75d585f21e8b32bf65d673b10f9
배해진 <bae05030@ajou.ac.kr>
2020-10-06 18:25:11 +0900

    ddori-v4 ddori.c

--- ddori.c
+++ ddori.c
@@ -14,230 +14,277 @@
 
 #define RAD 57.2958
 
+/* OSEK declarations */
 DeclareCounter(SysTimerCnt);
-
-/*Declare Resource*/
-DeclareResource(Move);
-DeclareResource(Turn);
-
-/*Declare Task*/
+DeclareEvent(MovementEvent);
+DeclareEvent(SteeringEvent);
 DeclareTask(Steering);
 DeclareTask(Movement);
 DeclareTask(Sonar);
 
 /* Global Value */
-int left_sonar_sensor = 15, right_sonar_sensor = 15; // Result of Median Filter
-int speed = 0;                                       //speed
+int left_sonar_sensor;
+int right_sonar_sensor;
 
 /* Ecrobot Device Initialize */
 void ecrobot_device_initialize()
 {
-  ecrobot_init_sonar_sensor(S3);
-  ecrobot_init_sonar_sensor(S4);
+	ecrobot_init_sonar_sensor(S3);
+	ecrobot_init_sonar_sensor(S4);
 }
 
 void ecrobot_device_terminate()
 {
-  ecrobot_term_sonar_sensor(S3);
-  ecrobot_term_sonar_sensor(S4);
+	ecrobot_term_sonar_sensor(S3);
+	ecrobot_term_sonar_sensor(S4);
 }
 
 /* LEJOS OSEK hook to be invoked from an ISR in category 2 */
 void user_1ms_isr_type2(void)
 {
-  StatusType ercd;
+	StatusType ercd;
 
-  ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
-  if (ercd != E_OK)
-    ShutdownOS(ercd);
+	ercd = SignalCounter(SysTimerCnt); /* Increment OSEK Alarm Counter */
+	if (ercd != E_OK)
+		ShutdownOS(ercd);
 }
 
 /* Auto-run Task -------------------------------------*/
 TASK(Sonar) //15ms마다 입력 받음
 {
-  //Static Value
-  static int left_sonar_array[5] = {15, 15, 15, 15, 15};
-  static int right_sonar_array[5] = {15, 15, 15, 15, 15};
-  static U8 entry_count = 0;
+	//Static Value
+	static int left_sonar_array[5] = { 15, 15, 15, 15, 15 }; //rigiht sensor input array (sliding window)
+	static int right_sonar_array[5] = { 15, 15, 15, 15, 15 }; //left sensor input array (sliding window)
 
-  //Declare Value
-  int left_sort[5], right_sort[5]; //Median Filter array
-  int i, j = 0;
-  int temp;
+															  //Value
+	static U8 entry_count = 0;
+	int i, j = 0;
+	int left_sort[5], right_sort[5]; //sorting array
+	int temp;
 
-  // Read Sensor
-  left_sonar_array[entry_count] = ecrobot_get_sonar_sensor(S3);
-  right_sonar_array[entry_count] = ecrobot_get_sonar_sensor(S4);
+	//get sensor input
+	left_sonar_array[entry_count] = ecrobot_get_sonar_sensor(S3);
+	right_sonar_array[entry_count] = ecrobot_get_sonar_sensor(S4);
 
-  // Sonar Sensor Correction
-  if (left_sonar_array[entry_count] >= 100)
-  {
-    left_sonar_array[entry_count] = 100;
-  }
-  if (right_sonar_array[entry_count] >= 100)
-  {
-    right_sonar_array[entry_count] = 100;
-  }
+	//Min Max calibrate
+	if (left_sonar_array[entry_count] >= 100)
+	{
+		left_sonar_array[entry_count] = 100;
+	}
+	if (right_sonar_array[entry_count] >= 100)
+	{
+		right_sonar_array[entry_count] = 100;
+	}
 
-  entry_count++;
+	//Median calibrate
+	for (i = 0; i < 5; i++)
+	{
+		left_sort[i] = left_sonar_array[i];
+		right_sort[i] = right_sonar_array[i];
+	}
 
-  // Sensor Median Filter
-  for (i = 0; i < 5; i++)
-  {
-    left_sort[i] = left_sonar_array[i];
-    right_sort[i] = right_sonar_array[i];
-  }
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5 - i; j++)
+		{
+			if (left_sort[j] > left_sort[j + 1])
+			{
+				temp = left_sort[j];
+				left_sort[j] = left_sort[j + 1];
+				left_sort[j + 1] = temp;
+			}
+			if (right_sort[j] > right_sort[j + 1])
+			{
+				temp = right_sort[j];
+				right_sort[j] = right_sort[j + 1];
+				right_sort[j + 1] = temp;
+			}
+		}
+	}
 
-  for (i = 0; i < 5; i++)
-  {
-    for (j = 0; j < 5 - i; j++)
-    {
-      if (left_sort[j] > left_sort[j + 1])
-      {
-        temp = left_sort[j];
-        left_sort[j] = left_sort[j + 1];
-        left_sort[j + 1] = temp;
-      }
-      if (right_sort[j] > right_sort[j + 1])
-      {
-        temp = right_sort[j];
-        right_sort[j] = right_sort[j + 1];
-        right_sort[j + 1] = temp;
-      }
-    }
-  }
-  // Result of Median Filter
-  left_sonar_sensor = left_sort[2];
-  right_sonar_sensor = right_sort[2];
+	//Median result
+	left_sonar_sensor = left_sort[2];
+	right_sonar_sensor = right_sort[2];
 
-  //entry_count reset
-  if (entry_count == 5)
-  {
-    entry_count = 0;
-  }
+	//Steering Event Set when angle is not 0
+	if (left_sonar_sensor - right_sonar_sensor != 0)
+	{
+		SetEvent(Steering, SteeringEvent);
+	}
+
+	//Movement Event Set when distance changed
+	int avg = (left_sonar_array[entry_count] + right_sonar_array[entry_count]) / 2;
+	int prev_avg = 0;
+	if (entry_count == 0)
+	{
+		prev_avg = (left_sonar_array[5] + right_sonar_array[5]) / 2;
+	}
+	else
+	{
+		prev_avg = (left_sonar_array[entry_count - 1] + right_sonar_array[entry_count - 1]) / 2;
+	}
+	if (avg - prev_avg != 0)
+	{
+		SetEvent(Movement, MovementEvent);
+	}
+
+	//Entry Count Rotate
+	entry_count++;
+	if (entry_count == 5)
+	{
+		entry_count = 0;
+	}
+
+	TerminateTask();
 }
 
 TASK(Steering)
 {
-  //Static Value
-  static int steering_count = 0;
-  static double sonar_width = 15.5;
-  static double previous_steer = 0.0;
+	// event waiting
+	while (WaitEvent(SteeringEvent) == E_OK)
+	{
+		ClearEvent(SteeringEvent);
 
-  //Value
-  double d = (double)left_sonar_sensor - (double)right_sonar_sensor;
-  double angle = atan2(d, sonar_width);
-  angle = angle * RAD; // Result of angle by difference of left_sonar_sensor and right_sonar_sensor
+		//static value
+		static int steering_count = 0;
+		static double previous_steer = 0.0;
 
-  // angle correction
-  if (angle >= 80 || angle <= -80)
-  {
-    angle = previous_steer;
-  }
+		//const value
+		const double sonar_width = 15.5;
 
-  if (d > 0 && angle > 45.0)
-  {
-    angle = 45.0;
-  }
-  else if (d < 0 && (angle < -45.0 || angle > 45.0))
-  {
-    angle = -45.0;
-  }
-  // set steer by angle
-  steering_count = nxt_motor_get_count(B);
-  previous_steer = angle;
-  if (angle > 25)
-  {
-    if (steering_count < angle * 2.4)
-    {
-      nxt_motor_set_speed(B, 34, 1);
-    }
-    else if (steering_count > angle * 2.4)
-    {
-      nxt_motor_set_speed(B, -36, 1);
-    }
-    else
-    {
-      nxt_motor_set_speed(B, 0, 1);
-    }
-  }
-  else
-  {
-    if (steering_count < angle * 2.0)
-    {
-      nxt_motor_set_speed(B, 30, 1);
-    }
-    else if (steering_count > angle * 2.0)
-    {
-      nxt_motor_set_speed(B, -32, 1);
-    }
-    else
-    {
-      nxt_motor_set_speed(B, 0, 1);
-    }
-  }
-  display_goto_xy(8, 4);
-  display_int(steering_count, 3);
-  display_goto_xy(12, 4);
-  display_int(angle, 3);
-  display_update();
+		//vaule
+		double d = (double)left_sonar_sensor - (double)right_sonar_sensor;
+		double angle = atan2(d, sonar_width);
+		angle = angle * RAD; //angle
 
-  TerminateTask();
+							 //angle Min Max calibration
+		if (angle >= 80 || angle <= -80)
+		{
+			angle = previous_steer;
+		}
+
+		if (d > 0 && angle > 45.0)
+		{
+			angle = 45.0;
+		}
+		else if (d < 0 && (angle < -45.0 || angle > 45.0))
+		{
+			angle = -45.0;
+		}
+
+		// diraction control by angle
+		steering_count = nxt_motor_get_count(B);
+		previous_steer = angle;
+		//angle over 25 
+		if (angle > 25)
+		{
+			if (steering_count < angle * 2.0)
+			{
+				nxt_motor_set_speed(B, 30, 1);
+			}
+			else if (steering_count > angle * 2.0)
+			{
+				nxt_motor_set_speed(B, -32, 1);
+			}
+			else
+			{
+				nxt_motor_set_speed(B, 0, 1);
+			}
+		}
+		//angle under 25 
+		else
+		{
+			if (steering_count < angle * 1.8)
+			{
+				nxt_motor_set_speed(B, 28, 1);
+			}
+			else if (steering_count > angle * 1.8)
+			{
+				nxt_motor_set_speed(B, -30, 1);
+			}
+			else
+			{
+				nxt_motor_set_speed(B, 0, 1);
+			}
+		}
+	}
 }
 TASK(Movement)
 {
-  //Constance Value
-  const int low = 40;
-  const int high = 90;
-  //Value
-  int left_sensor = left_sonar_sensor;
-  int right_sensor = right_sonar_sensor;
-  int avg = (left_sensor + right_sensor) / 2;
-  int diff = (avg - 15);
+	// event waiting
+	while (1)
+	{
+		WaitEvent(MovementEvent);
+		ClearEvent(MovementEvent);
+		//static value
+		static int speed = 0;
 
-  int tmp = 0;
-  // set speed by previous distance and current distance
-  if (diff > 18)
-  {
-    speed = high;
-    if (diff > 25)
-    {
-      tmp++;
-      speed = speed + tmp;
-    }
-    else
-    {
-      tmp--;
-      speed = speed + tmp;
-    }
-  }
-  else if (diff < -1)
-  {
-    tmp--;
-    speed = -low - tmp;
-  }
-  else if (diff > 1)
-  {
-    tmp++;
-    speed = low + tmp;
-  }
+		//const value
+		const int low = 47; //low mode speed
+		const int high = 92;//high mode speed
 
-  nxt_motor_set_speed(A, 0, 1);
-  nxt_motor_set_speed(C, 0, 1);
-  // set speed when too far
-  if (avg >= 100)
-    speed = 0;
+							//value
+		int left_sensor = left_sonar_sensor;
+		int right_sensor = right_sonar_sensor;
+		int avg = (left_sensor + right_sensor) / 2; //distance between control car and follow car
+		int diff = (avg - 15); //distance excluding braking distance
+		int tmp = 0;
 
-  nxt_motor_set_speed(A, -speed, 1);
-  nxt_motor_set_speed(C, -speed, 1);
+		// crash avoidance mode
+		if (avg >= 80)
+		{
+			nxt_motor_set_speed(A, 0, 1);
+			nxt_motor_set_speed(C, 0, 1);
+		}
 
-  display_goto_xy(0, 7);
-  display_int(diff, 3);
-  display_goto_xy(4, 7);
-  display_int(avg, 3);
-  display_goto_xy(8, 7);
-  display_int(speed, 3);
-  display_update();
+		else
+		{
+			//high speed mode
+			if (diff > 18)
+			{
+				speed = high;
+				if (diff > 25)
+				{
+					tmp++;
+					speed = speed + tmp;
+				}
+				else
+				{
+					tmp--;
+					speed = speed + tmp;
+				}
+			}
+			//low speed mose
+			else if (diff < -2)
+			{
+				speed = -low - 5;
+			}
+			else if (diff > 2)
+			{
+				tmp++;
+				speed = low + tmp;
+			}
 
-  TerminateTask();
+			//stop when braking distance
+			if (diff < 2 && diff > -2)
+			{
+				speed = 0;
+			}
+
+			// crash avoidance mode
+			if (left_sensor >= 95 || right_sensor >= 95)
+			{
+				speed = 0;
+			}
+
+			//motor speed control
+			nxt_motor_set_speed(A, 0, 1);
+			nxt_motor_set_speed(C, 0, 1);
+
+			nxt_motor_set_speed(A, -speed, 1);
+			nxt_motor_set_speed(C, -speed, 1);
+		}
+	}
+
+	TerminateTask();
 }
